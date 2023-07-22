3d92e5727375b1314d3229e173f1a46b282cb0a8
Jim Huang <jserv@ccns.ncku.edu.tw>
2023-07-23 03:17:50 +0800

    Decompress prebuilt Linux image

--- mk/external.mk
+++ mk/external.mk
@@ -4,15 +4,16 @@
 #   _DATA_SHA1 : the checksum of the content in _DATA
 
 # kernel
-KERNEL_DATA_URL = https://github.com/jserv/semu/raw/blob/Image
+KERNEL_DATA_URL = https://github.com/jserv/semu/raw/blob/Image.bz2
 KERNEL_DATA = Image
-KERNEL_DATA_SHA1 = 4ffc3bb847920187a19c48e1964fae618165333a
+KERNEL_DATA_SHA1 = e29659cc36420ea4d53e4b8641f5e8958e6f9624
 
 define download
 $($(T)_DATA):
 	$(VECHO) "  GET\t$$@\n"
 	$(Q)curl --progress-bar -O -L -C - "$(strip $($(T)_DATA_URL))"
-	$(Q)echo "$(strip $$($(T)_DATA_SHA1))  $$@" | $(SHA1SUM) -c
+	$(Q)echo "$(strip $$($(T)_DATA_SHA1)) $$@.bz2" | $(SHA1SUM) -c
+	$(Q)bunzip2 $$@.bz2
 endef
 
 EXTERNAL_DATA = KERNEL
