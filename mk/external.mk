f9e51dffbf5bb58fd91591824a5a98cb1c873134
Mes0903 <mes900903@gmail.com>
2026-03-25 19:10:16 +0800

    DO NOT MERGE: Add CI test blobs
    Use the temporary prebuilt DirectFB2 ext4 image from the review blob
    path for virtio-gpu CI.
    
    This commit will be removed once the normal blob path is updated.

--- mk/external.mk
+++ mk/external.mk
@@ -6,9 +6,9 @@
 COMMON_URL = https://github.com/sysprog21/semu/raw/blob
 
 # kernel
-KERNEL_DATA_URL = $(COMMON_URL)/Image.bz2
+KERNEL_DATA_URL = https://github.com/Mes0903/semu/raw/blob/Image.bz2
 KERNEL_DATA = Image
-KERNEL_DATA_SHA1 = f33badc277a88c17ce36b28d229a3f99cbccef91
+KERNEL_DATA_SHA1 = 0666dbb915cdb2275c73183caa45524b57e5ea7d
 
 # initrd
 INITRD_DATA_URL = $(COMMON_URL)/rootfs.cpio.bz2
@@ -16,7 +16,7 @@ INITRD_DATA = rootfs.cpio
 INITRD_DATA_SHA1 = a63336a28e484ed9cd560652c336b93affe50126
 
 # guest disk
-GUEST_DISK_DATA_URL = $(COMMON_URL)/ext4.img.bz2
+GUEST_DISK_DATA_URL = https://github.com/Mes0903/semu/raw/blob/ext4.img.bz2
 GUEST_DISK_DATA = ext4.img
 GUEST_DISK_DATA_SHA1 = 83ed49c16d341bdf3210141d5f6d5842b77a6adc
 
