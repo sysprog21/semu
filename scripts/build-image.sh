Mes0903/Mes-semu-dev

Add DirectFB2 guest disk build support
Add the DirectFB2 cross-compilation flow and a '--directfb-test'
build-image shortcut for producing an ext4 guest disk containing the
test program.

Since ext4.img now carries the DirectFB2 test program instead of being
an empty filesystem image, treat it like the other prebuilt guest
artifacts and download it from the blob path by default.

Co-authored-by: Shengwen Cheng <shengwen1997.tw@gmail.com>

scripts/build-image.sh
@@ -43,7 +43,7 @@ function do_buildroot
     safe_copy configs/buildroot.config buildroot/.config
     safe_copy configs/busybox.config buildroot/busybox.config
     cp -f target/init buildroot/fs/cpio/init
-    
+
     # Otherwise, the error below raises:
     #   You seem to have the current working directory in your
     #   LD_LIBRARY_PATH environment variable. This doesn't work.
@@ -52,6 +52,10 @@ function do_buildroot
     ASSERT make olddefconfig
     ASSERT make $PARALLEL
     popd
+
+    if [[ $BUILD_DIRECTFB_TEST -eq 1 ]]; then
+        do_extra_packages
+    fi
 
     if [[ $EXTERNAL_ROOT -eq 1 ]]; then
         echo "Copying rootfs.cpio to rootfs_full.cpio (external root mode)"
@@ -86,12 +90,13 @@ function do_linux
 
 function show_help {
     cat << EOF
-Usage: $0 [--buildroot] [--linux] [--all] [--external-root] [--clean-build] [--help]
+Usage: $0 [--buildroot] [--linux] [--directfb-test] [--all] [--external-root] [--clean-build] [--help]
 
 Options:
   --buildroot         Build Buildroot rootfs
+  --directfb-test     Build an ext4 guest disk with the DirectFB2 test payload
   --linux             Build Linux kernel
-  --all               Build both Buildroot and Linux
+  --all               Build both Buildroot and Linux kernel
   --external-root     Use external rootfs instead of initramfs
   --clean-build       Remove entire buildroot/ and/or linux/ directories before build
   --help              Show this message
@@ -100,6 +105,7 @@ EOF
 }
 
 BUILD_BUILDROOT=0
+BUILD_DIRECTFB_TEST=0
 BUILD_LINUX=0
 EXTERNAL_ROOT=0
 CLEAN_BUILD=0
@@ -108,6 +114,11 @@ while [[ $# -gt 0 ]]; do
     case "$1" in
         --buildroot)
             BUILD_BUILDROOT=1
+            ;;
+        --directfb-test)
+            BUILD_BUILDROOT=1
+            BUILD_DIRECTFB_TEST=1
+            EXTERNAL_ROOT=1
             ;;
         --linux)
             BUILD_LINUX=1
@@ -132,6 +143,63 @@ while [[ $# -gt 0 ]]; do
     esac
     shift
 done
+
+function do_directfb
+{
+    export PATH=$PATH:$PWD/buildroot/output/host/bin
+    export BUILDROOT_OUT=$PWD/buildroot/output/
+    export DIRECTFB_STAGE=$PWD/directfb
+    mkdir -p directfb
+
+    # Build DirectFB2
+    if [ ! -d DirectFB2 ]; then
+        echo "Cloning DirectFB2..."
+        ASSERT git clone https://github.com/directfb2/DirectFB2.git
+    else
+        echo "DirectFB2 already exists, skipping clone..."
+    fi
+    pushd DirectFB2
+    cp ../configs/riscv-cross-file .
+    ASSERT meson -Ddrmkms=true --cross-file riscv-cross-file build/riscv
+    ASSERT meson compile -C build/riscv
+    ASSERT env DESTDIR=$BUILDROOT_OUT/host/riscv32-buildroot-linux-gnu/sysroot meson install -C build/riscv
+    ASSERT env DESTDIR=$DIRECTFB_STAGE meson install -C build/riscv
+    popd
+
+    # Build DirectFB2 examples
+    if [ ! -d DirectFB-examples ]; then
+        echo "Cloning DirectFB-examples..."
+        ASSERT git clone https://github.com/directfb2/DirectFB-examples.git
+    else
+        echo "DirectFB-examples already exists, skipping clone..."
+    fi
+    pushd DirectFB-examples/
+    cp ../configs/riscv-cross-file .
+    ASSERT meson --cross-file riscv-cross-file build/riscv
+    ASSERT meson compile -C build/riscv
+    ASSERT env DESTDIR=$DIRECTFB_STAGE meson install -C build/riscv
+    popd
+}
+
+function do_extra_packages
+{
+    export PATH="$PWD/buildroot/output/host/bin:$PATH"
+    export CROSS_COMPILE=riscv32-buildroot-linux-gnu-
+
+    rm -rf extra_packages
+    mkdir -p extra_packages
+    mkdir -p extra_packages/root
+
+    do_directfb && OK
+
+    if ! find directfb -mindepth 1 -print -quit | grep -q .; then
+        echo "Error: DirectFB staging tree is empty."
+        exit 1
+    fi
+
+    ASSERT cp -r directfb/. extra_packages/
+    ASSERT cp target/run.sh extra_packages/root/
+}
 
 if [[ $BUILD_BUILDROOT -eq 0 && $BUILD_LINUX -eq 0 ]]; then
     echo "Error: No build target specified. Use --buildroot, --linux, or --all."

sysprog21/semu

scripts/build-image.sh
@@ -43,7 +43,7 @@ function do_buildroot
     safe_copy configs/buildroot.config buildroot/.config
     safe_copy configs/busybox.config buildroot/busybox.config
     cp -f target/init buildroot/fs/cpio/init
-    
+
     # Otherwise, the error below raises:
     #   You seem to have the current working directory in your
     #   LD_LIBRARY_PATH environment variable. This doesn't work.
@@ -52,6 +52,10 @@ function do_buildroot
     ASSERT make olddefconfig
     ASSERT make $PARALLEL
     popd
+
+    if [[ $BUILD_DIRECTFB_TEST -eq 1 ]]; then
+        do_extra_packages
+    fi
 
     if [[ $EXTERNAL_ROOT -eq 1 ]]; then
         echo "Copying rootfs.cpio to rootfs_full.cpio (external root mode)"
@@ -86,12 +90,13 @@ function do_linux
 
 function show_help {
     cat << EOF
-Usage: $0 [--buildroot] [--linux] [--all] [--external-root] [--clean-build] [--help]
+Usage: $0 [--buildroot] [--linux] [--directfb2-test] [--all] [--external-root] [--clean-build] [--help]
 
 Options:
   --buildroot         Build Buildroot rootfs
+  --directfb2-test     Build an ext4 guest disk with the DirectFB2 test payload
   --linux             Build Linux kernel
-  --all               Build both Buildroot and Linux
+  --all               Build both Buildroot and Linux kernel
   --external-root     Use external rootfs instead of initramfs
   --clean-build       Remove entire buildroot/ and/or linux/ directories before build
   --help              Show this message
@@ -100,6 +105,7 @@ EOF
 }
 
 BUILD_BUILDROOT=0
+BUILD_DIRECTFB_TEST=0
 BUILD_LINUX=0
 EXTERNAL_ROOT=0
 CLEAN_BUILD=0
@@ -108,6 +114,11 @@ while [[ $# -gt 0 ]]; do
     case "$1" in
         --buildroot)
             BUILD_BUILDROOT=1
+            ;;
+        --directfb2-test)
+            BUILD_BUILDROOT=1
+            BUILD_DIRECTFB_TEST=1
+            EXTERNAL_ROOT=1
             ;;
         --linux)
             BUILD_LINUX=1
@@ -132,6 +143,63 @@ while [[ $# -gt 0 ]]; do
     esac
     shift
 done
+
+function do_directfb
+{
+    export PATH=$PATH:$PWD/buildroot/output/host/bin
+    export BUILDROOT_OUT=$PWD/buildroot/output/
+    export DIRECTFB_STAGE=$PWD/directfb
+    mkdir -p directfb
+
+    # Build DirectFB2
+    if [ ! -d DirectFB2 ]; then
+        echo "Cloning DirectFB2..."
+        ASSERT git clone https://github.com/directfb2/DirectFB2
+    else
+        echo "DirectFB2 already exists, skipping clone..."
+    fi
+    pushd DirectFB2
+    cp ../configs/riscv-cross-file .
+    ASSERT meson -Ddrmkms=true --cross-file riscv-cross-file build/riscv
+    ASSERT meson compile -C build/riscv
+    ASSERT env DESTDIR=$BUILDROOT_OUT/host/riscv32-buildroot-linux-gnu/sysroot meson install -C build/riscv
+    ASSERT env DESTDIR=$DIRECTFB_STAGE meson install -C build/riscv
+    popd
+
+    # Build DirectFB2 examples
+    if [ ! -d DirectFB-examples ]; then
+        echo "Cloning DirectFB-examples..."
+        ASSERT git clone https://github.com/directfb2/DirectFB-examples
+    else
+        echo "DirectFB-examples already exists, skipping clone..."
+    fi
+    pushd DirectFB-examples/
+    cp ../configs/riscv-cross-file .
+    ASSERT meson --cross-file riscv-cross-file build/riscv
+    ASSERT meson compile -C build/riscv
+    ASSERT env DESTDIR=$DIRECTFB_STAGE meson install -C build/riscv
+    popd
+}
+
+function do_extra_packages
+{
+    export PATH="$PWD/buildroot/output/host/bin:$PATH"
+    export CROSS_COMPILE=riscv32-buildroot-linux-gnu-
+
+    rm -rf extra_packages
+    mkdir -p extra_packages
+    mkdir -p extra_packages/root
+
+    do_directfb && OK
+
+    if ! find directfb -mindepth 1 -print -quit | grep -q .; then
+        echo "Error: DirectFB staging tree is empty."
+        exit 1
+    fi
+
+    ASSERT cp -r directfb/. extra_packages/
+    ASSERT cp target/run.sh extra_packages/root/
+}
 
 if [[ $BUILD_BUILDROOT -eq 0 && $BUILD_LINUX -eq 0 ]]; then
     echo "Error: No build target specified. Use --buildroot, --linux, or --all."